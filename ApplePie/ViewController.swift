0cecbd8579a68b0f46f50ff86f820e15889d0600
Burak Donat <thedonat@burak-macbook-pro.local>
2019-01-29 20:51:58 +0300

    Initial Commit

---
+++ ApplePie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by Burak Donat on 29.01.2019.
+//  Copyright © 2019 Burak Donat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
