21e9b5e6bd154eabd92983245eaa8f422bfcfc65
Burak Donat <burak.donat@kgteknoloji.com>
2020-05-25 13:53:13 +0300

    first commit

---
+++ ApplePie/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Burak Donat on 31.01.2019.
+//  Copyright © 2019 Burak Donat. All rights reserved.
+//
+
+import Foundation
---
+++ ApplePie/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  ApplePie
+//
+//  Created by Burak Donat on 2.02.2019.
+//  Copyright © 2019 Burak Donat. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
